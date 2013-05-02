c7075c110249a2b3727284096f0b48568300c35a
Will Potter <will@wpotter.me>
2013-05-01 21:20:58 -0400

    updated to a better version of the yahoo api

--- stock.py
+++ stock.py
@@ -1,4 +1,6 @@
-import ystockquote
+import requests
+import urllib
+from bs4 import BeautifulSoup
 
 
 class Stock(object):
@@ -9,20 +11,18 @@ class Stock(object):
 		self.percent_change = 0
 
 	def get_percent_change(self, start_date, end_date):
-		self.data = ystockquote.get_historical_prices(self.quote, convert_date(start_date), convert_date(end_date))
-		days = len(self.data) - 1
-		p2 = float(self.data[1][4])
-		p1 = float(self.data[days][4])
+
+		# q = 'select * from yahoo.finance.quote where symbol in ("'+self.quote+'")'
+		q = 'select * from yahoo.finance.historicaldata where symbol = "'+self.quote+'" and startDate = "'+start_date+'" and endDate = "'+end_date+'"'
+		query = urllib.quote_plus(q)
+		url = "http://query.yahooapis.com/v1/public/yql?q="+query+"&env=http%3A%2F%2Fdatatables.org%2Falltables.env"
+		r = BeautifulSoup(requests.get(url).text)
+		quotes = r.find_all("quote")
+		p2 = float(quotes[0].close.string)
+		p1 = float(quotes[1].close.string)
 		self.percent_change = (p2 - p1)/(.5 * (p1 + p2)) * 100
 
 
-'''Converts dates from our standard format to '''
-def convert_date(date):
-	return date.replace("-","")
-
-
-
-
 stock = Stock("GOOG")
-stock.get_percent_change("2013-03-27", "2013-03-25")
+stock.get_percent_change("2013-03-25", "2013-03-26")
 print stock.percent_change
