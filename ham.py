28545901714b36eed7156ee07898ed804d6e7d50
Teddy Knox <teddyknox@gmail.com>
2013-05-16 01:08:58 -0400

    before commit

--- ham.py
+++ ham.py
@@ -1,9 +1,10 @@
 # Opting for a procedural approach
 
-from util import *
 from news import News
 from sklearn import svm
 import math
+import sqlite3
+from datetime import *
 import numpy as np
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -11,15 +12,13 @@ import requests
 import urllib
 import ystockquote
 from bs4 import BeautifulSoup
+from random import shuffle
 
+SYMBOLS = ['AAPL', 'GOOG', 'NFLX', 'TSLA', 'FB']
+BAG_OF_WORDS=0
 TRAIN_TEST_RATIO = 3 # e.g 3:1 would be 3
 
-def main():
-	vectorizer = TfidfBagOfWords()
-	model = svm.SVC(kernel='sigmoid', verbose=True)	
-	dry_run(model, vectorizer)	
-
-def dry_run(model, vectorizer): # messy code to test with movie reviews
+def dry_run(): # messy code to test with movie reviews
 
 	from nltk.corpus import movie_reviews
 	docs = [movie_reviews.raw(fileid) 
@@ -34,27 +33,45 @@ def dry_run(model, vectorizer): # messy code to test with movie reviews
 	training, testing = divide_corpus(docs)
 	training_labels, testing_labels = divide_corpus(labels)
 
+	vectorizer = TFIDFBagOfWords()
 	training_vectors = vectorizer.fit_transform(training, training_labels)
 	testing_vectors = vectorizer.transform(testing)
 
-	model.fit(training_vectors, training_labels)
-	print model.score(testing_vectors, testing_labels) # prints accuracy testing_vectors, testing_labels)	
+	model = train_model(training_vectors, training_labels)
+	results = test_model(model, testing_vectors, testing_labels)	
 
-def run(model, vectorizer):
+def double_shuffle(list1, list2): # shuffle two lists the same way. Used to shuffle both the labels and the docs in the dry run
+	list1_shuf = []
+	list2_shuf = []
+	index_shuf = range(len(list1))
+	shuffle(index_shuf)
+	for i in index_shuf:
+	    list1_shuf.append(list1[i])
+	    list2_shuf.append(list2[i])
+	return list1_shuf, list2_shuf
+
+def run():
 	news = News('Resources/articles.db')
 
+	print 'Reading articles from db... ',
 	raw = news.db_articles()
 
+	print 'done\nDividing corpus... ',
 	training, testing = divide_corpus(raw)
 
+	print 'done\nAssigning stock market labels to documents... ',
 	training, training_labels = corpus_labels(training)
 	testing, testing_labels = corpus_labels(testing)
 
-	training_vectors = vectorizer.fit_transform(training, training_labels)
-	testing_vectors = vectorizer.transform(testing)
+	print 'done\nExtracting features and vectorizing the corpus... ',
+	bag_of_words = BagOfWords()
+	training_vectors = bag_of_words.fit_transform(training, training_labels)
+	testing_vectors = bag_of_words.transform(testing)
 
-	model.fit(training_vectors, training_labels)
-	print model.score(testing_vectors, testing_labels) # prints accuracy
+	print 'done\nTraining SVM... ',
+	model = train_model(training_vectors, training_labels)
+	print 'done\nTesting SVM... done\n'
+	results = test_model(model, testing_vectors, testing_labels)
 
 
 def corpus_labels(corpus):
@@ -71,7 +88,7 @@ def corpus_labels(corpus):
 			label = change/math.fabs(change) # => 1 or -1
 			labels.append(label)
 		else:
-			pass # no stock
+			print 'no stock data'
 	return filtered_docs, np.array(labels, dtype=np.int8)
 
 def divide_corpus(A):
@@ -80,6 +97,11 @@ def divide_corpus(A):
 	B = A[:l-l/TRAIN_TEST_RATIO]
 	C = A[l-l/TRAIN_TEST_RATIO:]
 	return B, C
+
+def train_model(vectors, labels):
+	model = svm.SVC(kernel='linear', verbose=True)
+	model.fit(vectors, labels)
+	return model
 
 def test_model(model, vectors, labels):
 	preds = model.predict(vectors)
@@ -97,8 +119,92 @@ def test_model(model, vectors, labels):
 	acc = correct/total
 	print "%d/%d Correct" % (correct, total)
 	print "Accuracy: %.2f" % acc
-	print "Predictions: %d Positive, %d Negative" % (up_count, down_count)
+	print "Predictions: %d UP, %d DOWN" % (up_count, down_count)
 	return acc
+
+def convert_date(date):
+	return date.replace("-", "")
+
+def db_symbol_change(symbol, date):
+	daynum = date.weekday()
+	if daynum == 0: # if monday
+		prevdate = date - timedelta(days=3)
+
+	elif daynum == 5: # if saturday
+		prevdate = date - timedelta(days=1)
+		date += timedelta(days=2)
+
+	elif daynum == 6: # if sunday
+		prevdate = date - timedelta(days=2)
+		date += timedelta(days=1)
+
+	else: # if Tuesday - Friday
+		prevdate = date - timedelta(days=1)
+
+	# print "-"*30
+	# print date, "====>", date.weekday()
+	# print prevdate, "====>", prevdate.weekday()
+
+	date_str = date.strftime('%Y-%m-%d')
+	prevdate_str = prevdate.strftime('%Y-%m-%d')
+
+	db = sqlite3.connect('Resources/articles.db')
+	c = db.cursor()
+	r1 = list(c.execute("select price from quotes where symbol = '"+symbol+"' and date = '"+prevdate_str+"'"))
+	r2 = list(c.execute("select price from quotes where symbol = '"+symbol+"' and date = '"+date_str+"'"))
+
+	db.close()
+
+	if len(r1) == 0 or len(r2) == 0:
+		return False
+	else:
+		p1 = float(r1[0][0])
+		p2 = float(r2[0][0])
+		return (p2 - p1) / (.5 * (p1 + p2)) * 100
+
+	# print prevdate_str, p1, date_str, p2
+def yahoo_symbol_change(symbol, date):
+	"""Pulls data from Yahoo's API and calculates the percent change from the start data to the end date."""
+
+	daynum = date.weekday()
+	if daynum == 0: # if monday
+		prevdate = date - timedelta(days=3)
+
+	elif daynum == 5: # if saturday
+		prevdate = date - timedelta(days=1)
+		date += timedelta(days=2)
+
+	elif daynum == 6: # if sunday
+		prevdate = date - timedelta(days=2)
+		date += timedelta(days=1)
+
+	else: # if Tuesday - Friday
+		prevdate = date - timedelta(days=1)
+
+	date_str = date.strftime('%Y-%m-%d')
+	prevdate_str = prevdate.strftime('%Y-%m-%d')
+
+	q = 'select * from yahoo.finance.historicaldata where symbol = "%s" and startDate = "%s" and endDate = "%s"' % (symbol, date_str, prevdate_str)
+	query = urllib.quote_plus(q)
+
+	# Format URL for YQL
+	url = "http://query.yahooapis.com/v1/public/yql?q=" + query + "&env=http%3A%2F%2Fdatatables.org%2Falltables.env"
+
+	# Launch Yahoo Request
+	r = BeautifulSoup(requests.get(url).text)
+	symbols = r.find_all("symbol")
+
+	# If YQL Api is not down, simply calculate percent change
+	if(len(symbols) > 0): 
+		p2 = float(symbols[0].close.string)
+		p1 = float(symbols[1].close.string)
+		return (p2 - p1) / (.5 * (p1 + p2)) * 100
+	else: # Otherwise call the ystocksymbol package
+		self.data = ystockquote.get_historical_prices(symbol, convert_date(date_str), convert_date(prevdate_str))
+		days = len(self.data) - 1
+		p2 = float(self.data[1][4])
+		p1 = float(self.data[days][4])
+		return (p2 - p1) / (.5 * (p1 + p2)) * 100
 
 class Vectorizer(object): # Abstract class do not instantiate directly
 	def pre(self, corpus):
@@ -116,7 +222,7 @@ class BagOfWords(Vectorizer):
 	def __init__(self):
 		self.skvectorizer = CountVectorizer(stop_words=stopwords.words('english'), ngram_range=(1, 2), token_pattern=ur'\b\w+\b', min_df=1)
 
-class TfidfBagOfWords(Vectorizer):
+class TFIDFBagOfWords(Vectorizer):
 	def __init__(self):
 		self.skvectorizer = CountVectorizer(stop_words=stopwords.words('english'), ngram_range=(1, 2), token_pattern=ur'\b\w+\b', min_df=1)
 		self.sktransformer = TfidfTransformer()
@@ -131,4 +237,4 @@ class TfidfBagOfWords(Vectorizer):
 		vectors = self.skvectorizer.transform(docs)
 		return self.sktransformer.transform(vectors)
 
-main()
+run()
